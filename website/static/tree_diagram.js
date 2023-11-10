2e9259d883428f103fdac084cc89d472e33ecf5b
Jeremiah Giordani <jeremiah@dynamic-oit-vapornet100-10-8-28-54.princeton.edu>
2023-11-10 09:30:11 -0500

    Tree diagram is drawn from input boxes

--- website/static/tree_diagram.js
+++ website/static/tree_diagram.js
@@ -15,94 +15,129 @@ document.addEventListener('DOMContentLoaded', function() {
   const treemap = d3.tree().size([height, width]);
 
   // Sample data for the tree diagram
-  const treeData = {
+  // const treeData = {
+  //     name: "Total Population",
+  //     children: [
+  //         { name: "Computer Science (20)" },
+  //         { name: "Electrical Engineering (15)" },
+  //         { name: "Mechanical Engineering (25)" },
+  //         { name: "Biomedical Engineering (10)" }
+  //     ]
+  // };
+
+  function updateGraph() {
+    const subpopulations = [
+      {
+        name: document.getElementById('subpopulation1').value + " (" + document.getElementById('count1').value + ")",
+      },
+      {
+        name: document.getElementById('subpopulation2').value + " (" + document.getElementById('count2').value + ")",
+      },
+      {
+        name: document.getElementById('subpopulation3').value + " (" + document.getElementById('count3').value + ")",
+      },
+    ];
+  
+    // Filter out any subpopulations that don't have a name or count
+    const filteredSubpopulations = subpopulations.filter(sp => sp.name !== " ()");
+
+    const treeData = {
       name: "Total Population",
-      children: [
-          { name: "Computer Science (20)" },
-          { name: "Electrical Engineering (15)" },
-          { name: "Mechanical Engineering (25)" },
-          { name: "Biomedical Engineering (10)" }
-      ]
-  };
+      children: filteredSubpopulations
+    };
 
-  // Assigns parent, children, height, depth
-  const root = d3.hierarchy(treeData, function(d) { return d.children; });
-  treemap(root);
+    // Clear the previous graph
+    svg.selectAll('*').remove();
 
-  // Add links between nodes
-  svg.selectAll(".link")
-      .data(root.links())
-      .enter().append("path")
-      .attr("class", "link")
-      .attr("fill", "none")
-      .attr("stroke", "#ccc")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.linkHorizontal()
-          .x(function(d) { return d.y; })
-          .y(function(d) { return d.x; }));
+    // Assigns the structure and recalculates the layout
+    const root = d3.hierarchy(treeData, function(d) { return d.children; });
+    treemap(root);
 
-  // Add each node as a group
-  const node = svg.selectAll(".node")
-      .data(root.descendants())
-      .enter().append("g")
-      .attr("class", function(d) { 
-          return "node" + (d.children ? " node--internal" : " node--leaf"); 
-      })
-      .attr("transform", function(d) {
-          return "translate(" + d.y + "," + d.x + ")";
-      });
+    // // Assigns parent, children, height, depth
+    // const root = d3.hierarchy(treeData, function(d) { return d.children; });
+    // treemap(root);
 
-  // Add a circle to represent each node
-  const nodeEnter = node.append("circle")
-      .attr("r", 0) // initial radius 0
-      .attr("fill", "#fff")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5);
+    // Add links between nodes
+    svg.selectAll(".link")
+        .data(root.links())
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.linkHorizontal()
+            .x(function(d) { return d.y; })
+            .y(function(d) { return d.x; }));
 
-  // Transition nodes to their new size
-  nodeEnter.transition()
-      .duration(750)
-      .attr("r", 7);
+    // Add each node as a group
+    const node = svg.selectAll(".node")
+        .data(root.descendants())
+        .enter().append("g")
+        .attr("class", function(d) { 
+            return "node" + (d.children ? " node--internal" : " node--leaf"); 
+        })
+        .attr("transform", function(d) {
+            return "translate(" + d.y + "," + d.x + ")";
+        });
 
-  // Add a label to each node
-  node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", function(d) { 
-          return d.children ? -8 : 8; 
-      })
-      .style("text-anchor", function(d) {
-          return d.children ? "end" : "start";
-      })
-      .text(function(d) { return d.data.name; })
-      .style("fill-opacity", 0)
-      .transition()
-      .duration(750)
-      .style("fill-opacity", 1);
+    // Add a circle to represent each node
+    const nodeEnter = node.append("circle")
+        .attr("r", 0) // initial radius 0
+        .attr("fill", "#fff")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5);
+
+    // Transition nodes to their new size
+    nodeEnter.transition()
+        .duration(750)
+        .attr("r", 7);
+
+    // Add a label to each node
+    node.append("text")
+        .attr("dy", "0.31em")
+        .attr("x", function(d) { 
+            return d.children ? -8 : 8; 
+        })
+        .style("text-anchor", function(d) {
+            return d.children ? "end" : "start";
+        })
+        .text(function(d) { return d.data.name; })
+        .style("fill-opacity", 0)
+        .transition()
+        .duration(750)
+        .style("fill-opacity", 1);
 
 
-// Variable to keep track of the currently selected node
-let selectedNode = null;
+    // Variable to keep track of the currently selected node
+    let selectedNode = null;
 
-// Add a click event listener to each node
-node.on('click', function(event, d) {
-  // Check if it is a leaf node
-  if (!d.children) {
-    // If there is a selected node and it's this node, deselect it
-    if (selectedNode === this) {
-      d3.select(this).select('circle')
-        .style('fill', '#fff'); // Change color back to white
-      selectedNode = null; // Deselect node
-    } else {
-      // If there is a selected node, change its color back to white
-      if (selectedNode) {
-        d3.select(selectedNode).select('circle')
-          .style('fill', '#fff');
+    // Add a click event listener to each node
+    node.on('click', function(event, d) {
+      // Check if it is a leaf node
+      if (!d.children) {
+        // If there is a selected node and it's this node, deselect it
+        if (selectedNode === this) {
+          d3.select(this).select('circle')
+            .style('fill', '#fff'); // Change color back to white
+          selectedNode = null; // Deselect node
+        } else {
+          // If there is a selected node, change its color back to white
+          if (selectedNode) {
+            d3.select(selectedNode).select('circle')
+              .style('fill', '#fff');
+          }
+          // Select this node and change its color to red
+          d3.select(this).select('circle')
+            .style('fill', 'red');
+          selectedNode = this; // Update the currently selected node
+        }
       }
-      // Select this node and change its color to red
-      d3.select(this).select('circle')
-        .style('fill', 'red');
-      selectedNode = this; // Update the currently selected node
-    }
+    });
   }
-});
+
+  // Event listener for the draw button
+  document.getElementById('draw-button').addEventListener('click', updateGraph);
+
+  // Initially, call updateGraph to draw the base graph
+  updateGraph();
 });
