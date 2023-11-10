4892906f165237bb59e6e626bea20ebfe24249f6
Ziyang Xu <ziyangx@cs.princeton.edu>
2023-11-10 17:58:06 +0000

    update highlight

--- website/static/app.js
+++ website/static/app.js
@@ -1,7 +1,9 @@
-$("body").on("click", "td", function() {
-    console.log("called correctly");
-    var index = $(this).index();
-    $("tr").each(function() {
-        $(this).children("td").eq(index).toggleClass("highlight");
+function registering_click() {
+    $("body").on("click", "td", function() {
+        console.log("called correctly");
+        var index = $(this).index();
+        $("tr").each(function() {
+            $(this).children("td").eq(index).toggleClass("highlight");
+        });
     });
-});
+}
