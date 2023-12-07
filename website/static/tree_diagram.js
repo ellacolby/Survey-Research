788becbc71ce1d8743939a2f5318e1c9832934da
Ella Colby <ellacolby@princeton.edu>
2023-12-07 11:42:18 -0500

    new implementation - vis.js instead of d3

--- website/static/tree_diagram.js
+++ website/static/tree_diagram.js
@@ -1,120 +1,104 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Set the dimensions and margins of the diagram
-    const margin = {top: 20, right: 200, bottom: 20, left: 200};
-    const width = document.getElementById('tree-diagram').clientWidth - margin.left - margin.right;
-    const height = 900 - margin.top - margin.bottom;
-
-    // Declare variable to store the clicked column
-    let clickedColumn;
-    let clickedNode;
-  
-    // Append the svg object to the div with id "tree-diagram"
-    const svg = d3.select("#tree-diagram").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-    // Create the tree layout
-    const treemap = d3.tree().size([height, width]);
-  
-    function drawTree(treeData) {
-        // Create a hierarchy from the data
-        let root = d3.hierarchy(treeData);
+    function drawTree(TreeData) {
+        // Create an array to hold the nodes
+        let nodes = [];
     
-        // Create a tree layout
-        let treeLayout = d3.tree().size([width, height]);
+        // Create an array to hold the edges
+        let edges = [];
     
-        // Generate the tree
-        treeLayout(root);
+        // Recursive function to process the TreeData
+        function processNode(node, parentId = null) {
+        // Create a new node
+        let newNode = {
+            id: node.name,
+            label: `${node.name} (${node.count})`,
+        };
     
-        // Select the SVG element
-        let svg = d3.select('svg');
+        // Add the node to the nodes array
+        nodes.push(newNode);
     
-        // Draw the links (lines between nodes)
-        svg.selectAll('.link')
-            .data(root.links())
-            .enter()
-            .append('path')
-            .attr('class', 'link')
-            .attr('d', d3.linkVertical()
-                .x(d => d.x)
-                .y(d => d.y));
+        // If this node has a parent, create an edge
+        if (parentId) {
+            edges.push({
+            from: parentId,
+            to: node.name,
+            });
+        }
     
-        // Draw the nodes
-        svg.selectAll('.node')
-            .data(root.descendants())
-            .enter()
-            .append('circle')
-            .attr('class', 'node')
-            .attr('cx', d => d.x)
-            .attr('cy', d => d.y)
-            .attr('r', 5);
+        // Process the children
+        node.children.forEach((child) => processNode(child, node.name));
+        }
     
-        // Add labels to the nodes
-        svg.selectAll('.label')
-            .data(root.descendants())
-            .enter()
-            .append('text')
-            .attr('class', 'label')
-            .attr('x', d => d.x)
-            .attr('y', d => d.y)
-            .text(d => d.data.name);
+        // Start processing with the root node
+        processNode(TreeData);
+    
+        // Create a network
+        let container = document.getElementById("mynetwork");
+        let data = {
+        nodes: new vis.DataSet(nodes),
+        edges: new vis.DataSet(edges),
+        };
+        let options = {};
+        let network = new vis.Network(container, data, options);
     }
-  
-    // Draw the initial tree with just the Total Population node
-    drawTree({ name: "Total Population", children: [] });
 
-    // Add event listener to column headers
-    document.querySelectorAll('.column-header').forEach(header => {
-        header.addEventListener('click', () => {
-            // Store the clicked column
-            clickedColumn = header.textContent;
-
-            // Create a new tree data with the entries as children of the root node
-            let treeData = {
-                name: header.textContent,
-                children: entries.map(entry => ({ name: entry }))
-            };
-
-            // Create a new object with the key being the column header and the value being null
-            let obj = {};
-            obj[header.textContent] = null;
-        
-            // Push this object onto the stack
-            stack.push(obj);
-            });
-
-            drawTree(treeData)
-    });
-
-    // Add event listener to nodes
-    document.querySelectorAll('.node').forEach(node => {
-        node.addEventListener('click', () => {
-            clickedNode = node;
-            let poppedNode;
-
-            if (node === root) {
-                let treeData = {
-                    name: "Total Population",
-                    children: []
-                };
-                drawTree(treeData)
-                return;
+    function colClicked(colIndex) {
+        // Create an object to hold the counts
+        let counts = {};
+      
+        // Get the DataTable
+        let table = $('#myTable').DataTable();
+      
+        // Get the data for the clicked column
+        let columnData = table.column(colIndex).data().toArray();
+      
+        // Iterate over each value in the column
+        columnData.forEach(function(cellValue) {
+          // If the cell value is not empty
+          if (cellValue) {
+            // If this value is not already in the counts object, add it
+            if (!counts[cellValue]) {
+              counts[cellValue] = {
+                name: cellValue,
+                count: 0,
+                children: []
+              };
             }
+      
+            // Increment the count
+            counts[cellValue].count++;
+          }
+        });
+      
+        // Convert the counts object to an array of TreeData objects
+        let treeDataArray = Object.values(counts);
+      
+        // Clear the children of the root node
+        root.children = [];
+      
+        // Add the new TreeData objects as children of the root
+        root.children = root.children.concat(treeDataArray);
+      
+        // Draw the tree
+        drawTree(root);
+      }
 
-            // Pop elements from the stack until you find an object whose key equals the clicked node's key
-            do {
-                poppedNode = stack.pop();
-            } while (poppedNode && !poppedNode.hasOwnProperty(node.key));
+    let TreeData = {
+        name: "Total Population",
+        count: 1,
+        children: []
+    };
 
-            if (JSON.stringify(poppedNode) === JSON.stringify(node)) {
-                // Update the value of this object to be the node label
-                poppedNode[node.key] = node.label;
+    let root = TreeData
+      
+  // Call the function with your TreeData
+  drawTree(TreeData);
 
-                // Push it back onto the stack
-                stack.push(poppedNode);
-            }
-            });
-    });
+  $(document).ready(function() {
+    $('#myTable').DataTable();
+  });
+
+  $("#myTable th").click(function() {
+    colClicked($(this).index());
+  });
 });
